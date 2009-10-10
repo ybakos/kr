25db36e20454db0aabbc47c9d5c4fcd07fee85bc
Yong Bakos <projects@humanoriented.com>
2009-10-10 13:25:11 -0600

    Adding code for 1.7 functions

--- 1/1.7-1.15.c
+++ 1/1.7-1.15.c
@@ -2,12 +2,30 @@
   Name: 1.7 1.15
   Copyright: 2009
   Author: Yong Bakos
-  Date: 05/10/09 XX:XXAM
+  Date: 10/10/09 01:24PM
   Description: page 27
 */
 #include <stdio.h>
 
+double f_to_celsius(int);
+
 int main() {
- 
+  float f;
+  int lower, upper, step;
+  lower = 0;
+  upper = 300;
+  step = 20;
+  f = lower;
+  printf("%2c\t%4c\n", 'F', 'C');
+  while (f <= upper) {
+    //c = (5 * (f - 32)) / 9;
+    printf("%3.0f\t%6.2f\n", f, f_to_celsius(f));
+    f = f + step;
+  }
+  printf("Hello There.");
   return 0;
+}
+
+double f_to_celsius(int f) {
+    return (5.0/9.0) * (f - 32);
 }
--- 1/1.7.c
+++ 1/1.7.c
@@ -2,12 +2,29 @@
   Name: 1.7 Functions
   Copyright: 2009
   Author: Yong Bakos
-  Date: 05/10/09 XX:XXAM
+  Date: 10/10/09 01:14PM
   Description: page 24
 */
 #include <stdio.h>
 
+int power(int m, int n); // int power(int, int); prototypes don't care 'bout var names
+
 int main() {
- 
+  int i;
+  
+  for (i = 0; i < 10; ++i) {
+    printf("%d %d %d\n", i, power(2, i), power(-3, i));
+  }
   return 0;
+}
+
+// power: raise base to n-th power: n >= 0
+int power(int base, int n) {
+  int i, p;
+  p = 1;
+  
+  for (i = 1; i <= n; ++i)
+    p = p * base;
+
+  return p;
 }
