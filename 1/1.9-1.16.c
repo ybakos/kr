7c38744e740fc505a30d42db684f7c87f3f78e6e
Yong Bakos <projects@humanoriented.com>
2009-10-11 15:05:07 -0600

    Adding blank exercises for 1.9

---
+++ 1/1.9-1.16.c
@@ -0,0 +1,11 @@
+/*
+  Name: 1.9 1-16 
+  Copyright: 2009
+  Author: Yong Bakos
+  Date: 11/10/09 XXXPM
+  Description: page 30
+*/
+#include <stdio.h>
+
+int main() {
+}
---
+++ 1/1.9-1.19.c
@@ -0,0 +1,11 @@
+/*
+  Name: 1.9 1-17
+  Copyright: 2009
+  Author: Yong Bakos
+  Date: 11/10/09 XXXPM
+  Description: page 31
+*/
+#include <stdio.h>
+
+int main() {
+}
