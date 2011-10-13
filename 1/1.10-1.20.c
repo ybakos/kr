49b68549a50b98d03aa02cfac353633fffd76765
Yong Bakos <ybakos@humanoriented.com>
2011-10-13 11:42:11 -0600

    Adding solution for 1-20.

---
+++ 1/1.10-1.20.c
@@ -0,0 +1,35 @@
+/*
+  Name: 1.10 1-20
+  Copyright: 2011
+  Author: Yong Bakos
+  Date: 10/13/11 10:43AM
+  Description: page 34. This program replaces tabs with spaces.
+*/
+#include <stdio.h>
+
+#define TAB_WIDTH 8
+
+int getline(char s[], int lim);
+void detab(char s[], int width);
+
+int main() {
+  int c, numberOfSpaces, position;
+  position = 0;
+  while ( (c = getchar()) != EOF) {
+    if (c == '\t') {
+      while (position % (TAB_WIDTH) != 0) {
+        putchar(' ');
+        ++position;
+      }
+
+    } else if (c == '\n') {
+      putchar(c);
+      position = 0;
+    } else {
+      putchar(c);
+      ++position;
+    }
+  }
+
+  return 0;
+}
