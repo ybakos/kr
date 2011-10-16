9effd67645656bed5462937b7efb290e6e174f61
Yong Bakos <ybakos@humanoriented.com>
2011-10-16 17:18:32 -0600

    Adding empty stub for 1-22.

---
+++ 1/1.10-1.22.c
@@ -0,0 +1,18 @@
+/*
+  Name: 1.10 1-22
+  Copyright: 2011
+  Author: Yong Bakos
+  Date: 10/16/11 05:17PM
+  Description: page 34.
+    Write a program to "fold" long input lines into two or more shorter lines after the
+    last non-blank character that occurs before the n -th column of input. Make sure 
+    your program does something intelligent with very long lines, and if there are no 
+    blanks or tabs before the specified column. 
+*/
+#include <stdio.h>
+
+int main() {
+
+
+  return 0;
+}
