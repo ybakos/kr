8a6f936ce4011eb91b5b83f2e2697d4b8bb270f9
Yong Bakos <ybakos@humanoriented.com>
2011-10-16 17:23:51 -0600

    Adding getline and defines to 1-22.

--- 1/1.10-1.22.c
+++ 1/1.10-1.22.c
@@ -11,8 +11,24 @@
 */
 #include <stdio.h>
 
+#define MAXLENGTH 1000
+#define LINEWIDTH 70
+
+int getline(char s[], int lim);
+
 int main() {
 
-
   return 0;
+}
+
+int getline(char s[], int lim) {
+  int c, i;
+  for (i = 0; (i < lim - 1) && ((c = getchar()) != EOF) && (c != '\n'); ++i)
+    s[i] = c;
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
 }
